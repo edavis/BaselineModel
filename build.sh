417696b8805e4eafbac2a73c90c07a8eaaacbd95
Eric Davis <edavis@eresources.com>
2018-12-15 18:31:51 -0800

    build.sh: refactor

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+# clear out ratings.db
 sqlite3 ratings.db <<<'delete from ratings'
+sqlite3 ratings.db <<<'delete from stats'
+
+# download latest games
 rm -f results.csv && make results.csv
+
+# add ratings to ratings.db
 python updateRatings.py
-( cd www/ && hugo )
+
+# generate a new ratings post for each day in the database
+python generatePages.py
+
+# rebuild the site
+( cd www/ && rm -rf public/* && hugo )
